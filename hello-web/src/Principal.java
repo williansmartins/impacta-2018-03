a8803b223eb74b391bfb2f90c0266656990e9f6c
no4016@CLASSROOM.NET <no4016@CLASSROOM.NET>
2018-03-14 22:47:15 -0300

    Alteração

--- hello-web/src/Principal.java
+++ hello-web/src/Principal.java
@@ -2,7 +2,7 @@
 public class Principal {
 	
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
+		System.out.println("Olá CAIQUE");
 		Principal principal = new Principal();
 		principal.metodoEdgar();
 	}
