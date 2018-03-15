1e36941eef7d69b9617818f772d56427ed15fccd
phfsistemas <edgar.frastrone@gmail.com>
2018-03-14 22:37:17 -0300

    add

--- hello-web/src/Principal.java
+++ hello-web/src/Principal.java
@@ -3,6 +3,8 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		System.out.println("Olá mundo");
+		Principal principal = new Principal();
+		principal.metodoEdgar();
 	}
 	
 	public void metodoWillians(){
@@ -10,6 +12,7 @@ public class Principal {
 	}
 	
 	public void metodoEdgar(){
+		System.out.println("Edgar teste");
 	}
 	
 	public void metodoNarc(){
