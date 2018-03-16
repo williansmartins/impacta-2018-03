2fdb2b2660426e0f6b94e9ee77c930e05936a09e
phfsistemas <edgar.frastrone@gmail.com>
2018-03-15 21:29:19 -0300

    Add Edgar

---
+++ hello-web/src/br/com/impacta/javaweb/servlets/HelloServlet.java
@@ -0,0 +1,33 @@
+package br.com.impacta.javaweb.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(description = "Representa um Servlet de Boas Vindas!", urlPatterns = { "/HelloServlet" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+        // TODO Auto-generated constructor stub
+    	super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
