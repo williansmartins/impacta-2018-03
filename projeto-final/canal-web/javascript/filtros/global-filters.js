b482238dbd9392bee7fa21384c4d289dbbf1f29f
wsms <wsms@gft.com>
2018-03-26 16:15:10 -0300

    add template - gacp2

--- projeto-final/canal-web/javascript/filtros/global-filters.js
+++
@@ -1,8 +0,0 @@
-angular.module('controlei')
-.filter('myDateFormat', function myDateFormat($filter){
-  return function(text){
-  	console.info(text);
-    var tempdate = new Date(text.replace(/-/g,"/")); 
-    return $filter('date')(tempdate, "dd/MM/yyyy");
-  }
-});
