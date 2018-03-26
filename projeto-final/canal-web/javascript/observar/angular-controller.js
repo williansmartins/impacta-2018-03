897cb370a34f270acff701777ae19802af4c1181
wsms <wsms@gft.com>
2018-03-26 15:38:28 -0300

    organizando - gacp2

--- projeto-final/canal-web/javascript/observar/angular-controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('myApp')
-
-.controller('MyController', ['$scope','currencyConverter', function ($scope, currencyConverter) {
-  $scope.greeting = 'Hola!';
-  $scope.valor = currencyConverter.currencies;
-}]);
