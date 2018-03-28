e432eb2c700fabf9d542b46e6709f117083429c6
phfsistemas <edgar.frastrone@gmail.com>
2018-03-28 19:45:04 -0300

    Adicionado controller servico

--- projeto-cabelereiro-back/src/main/java/br/com/impacta/controller/UsuarioController.java
+++ projeto-cabelereiro-back/src/main/java/br/com/impacta/controller/UsuarioController.java
@@ -1,10 +1,15 @@
 package br.com.impacta.controller;
 
-import br.com.impacta.dao.JpaGenericDao;
-import br.com.impacta.model.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.impacta.dao.JpaGenericDao;
+import br.com.impacta.model.Servico;
+
+@Controller
+@RequestMapping("/cabelereiro/servico")
 public class UsuarioController {
 
-	JpaGenericDao<Usuario> dao = new JpaGenericDao<Usuario>();
+	JpaGenericDao<Servico> dao = new JpaGenericDao<Servico>();
 	
 }
