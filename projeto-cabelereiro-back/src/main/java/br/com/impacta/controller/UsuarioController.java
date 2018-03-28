a5bd70d730d668f0b5508262ce924065ce4519e2
CaiqueMacedof <caiquemacedof@yahoo.com.br>
2018-03-27 22:19:27 -0300

    Adicionando package Controller.

---
+++ projeto-cabelereiro-back/src/main/java/br/com/impacta/controller/UsuarioController.java
@@ -0,0 +1,10 @@
+package br.com.impacta.controller;
+
+import br.com.impacta.dao.JpaGenericDao;
+import br.com.impacta.model.Usuario;
+
+public class UsuarioController {
+
+	JpaGenericDao<Usuario> dao = new JpaGenericDao<Usuario>();
+	
+}
