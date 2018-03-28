ec1f0191d2d99b59eb062600b9e3ac165bdbd2b6
CaiqueMacedof <caiquemacedof@yahoo.com.br>
2018-03-27 23:03:50 -0300

    Controler Usuario.

--- projeto-cabelereiro-back/src/main/java/br/com/impacta/controller/UsuarioController.java
+++ projeto-cabelereiro-back/src/main/java/br/com/impacta/controller/UsuarioController.java
@@ -1,10 +1,33 @@
 package br.com.impacta.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import br.com.impacta.dao.JpaGenericDao;
+import br.com.impacta.model.TipoUsuario;
 import br.com.impacta.model.Usuario;
 
+@Controller
+@RequestMapping("/usuario")
 public class UsuarioController {
 
 	JpaGenericDao<Usuario> dao = new JpaGenericDao<Usuario>();
 	
+	@RequestMapping(value="/inserir", method=RequestMethod.POST)
+	@ResponseBody
+	public Usuario inserir(@RequestBody Usuario entrada) {
+		dao.insert(entrada);
+		return entrada;
+	}
+	
+	@RequestMapping(value="/buscar", method=RequestMethod.GET)
+	@ResponseBody
+	public List<Usuario> buscar() {
+		return dao.findAll();
+	}
 }
